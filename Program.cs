9b959061b696ff5c8d567c02520633eb9a6ef1da
Will <doggy.huang@gmail.com>
2023-04-09 22:46:48 +0800

    完成 LINELoginIDTokenValidator 開發

--- Program.cs
+++ Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using IdentityModel.Client;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+var tokenOption = new Option<string>(
+            name: "--token",
+            description: "The LINE Login's ID Token.");
+
+var channelIdOption = new Option<string>(
+            name: "--channel-id",
+            description: "The LINE Login's Channel ID.");
+
+var rootCommand = new RootCommand("LINE Login ID Token Validator");
+rootCommand.AddOption(tokenOption);
+rootCommand.AddOption(channelIdOption);
+
+rootCommand.SetHandler(async (idToken, channelId) =>
+    {
+        var certs = await GetJWKsCerts();
+
+        var keys  = new JsonWebKeySet(certs).GetSigningKeys();
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var decodedToken = tokenHandler.ReadJwtToken(idToken);
+
+        var key = keys.FirstOrDefault(k => k.KeyId == decodedToken.Header.Kid);
+
+        var handler = new JsonWebTokenHandler();
+        TokenValidationResult result = handler.ValidateToken(idToken, new TokenValidationParameters
+        {
+            ValidIssuer = "https://access.line.me",
+            ValidAudience = channelId, // LINE Login 的 Channel ID
+            IssuerSigningKey = key,
+        });
+
+        if (result.IsValid)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The ID Token is VALID.");
+            Console.ResetColor();
+        }
+        else
+        {
+            // https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/wiki/PII
+            Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+
+            // console output with color red
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The ID Token is INVALID: " + result.Exception.Message);
+            Console.ResetColor();
+        }
+    },
+    tokenOption, channelIdOption);
+
+return await rootCommand.InvokeAsync(args);
+
+// ----------------------------------------------
+
+static async Task<string> GetJWKsCerts()
+{
+    var http = new HttpClient();
+
+    // dotnet add package IdentityModel
+    var disco = await http.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+    {
+        Address = "https://access.line.me", // Authority
+        Policy = new() { ValidateEndpoints = false }
+    });
+
+    var certs = await http.GetStringAsync(disco.JwksUri);
+
+    return certs;
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# LINELoginIdTokenValidator
+
+## 初始化專案
+
 ```sh
 dotnet new console -n LINELoginIDTokenValidator
 cd LINELoginIDTokenValidator
@@ -13,4 +17,21 @@ dotnet add package System.CommandLine --prerelease
 git add .
 git commit -m "Add System.CommandLine package"
 
+dotnet add package IdentityModel
+dotnet add package System.IdentityModel.Tokens.Jwt
 ```
+
+## 使用方式
+
+1. 先透過 LINE Login 取得以 ES256 演算法簽發的 ID Token 字串
+
+    要讓 LINE Login 發出 `ES256` 簽發的 ID Token 必須要認真看完我的 [如何將一個 ASP.NET Core 網站快速加入 LINE Login 功能 (OpenID Connect)](https://blog.miniasp.com/post/2022/04/08/LINE-Login-with-OpenID-Connect-in-ASPNET-Core) 文章，確保從 Token Endpoint 取得 Access Token 與 ID Token 時，要額外送出一個 `id_token_key_type=JWK` 的參數才行！
+
+2. 使用此工具驗證 ID Token 的有效性
+
+    ```sh
+    dotnet run -- --channel-id "YOUR_CHANNEL_ID" --token "YOUR_ID_TOKEN"
+    ```
+
+    ![image](https://user-images.githubusercontent.com/88981/230779870-6dfb7dcc-2142-4410-850b-df2c3722de60.png)
+
